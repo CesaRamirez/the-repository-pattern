a1c8147885b01fcaebd638be07db4577d2de4c4e
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-01-14 20:44:09 -0600

    :package: update to bootstrap 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
+require("dotenv").config();
 
 /*
  |--------------------------------------------------------------------------
@@ -13,3 +14,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.browserSync(process.env.APP_URL)
